723d5a03aa6b73b3046b23b65230f32a3a30b07c
Emmanuel Otoo <eotoo218@gmail.com>
2026-06-04 13:37:54 +0000

    fix: Reorder import statements for consistency in scrapers module

--- src/sakai_bot/scrapers/__init__.py
+++ src/sakai_bot/scrapers/__init__.py
@@ -1,8 +1,8 @@
 """Sakai scrapers module."""
 
-from sakai_bot.scrapers.courses import CourseScraper
 from sakai_bot.scrapers.announcements import AnnouncementScraper
 from sakai_bot.scrapers.assignments import AssignmentScraper
+from sakai_bot.scrapers.courses import CourseScraper
 from sakai_bot.scrapers.exams import ExamDetector
 
 __all__ = [
