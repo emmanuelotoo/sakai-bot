d72384a1183d65ac1cd14f650ff89c1be405e8dd
Emmanuel Otoo <eotoo218@gmail.com>
2026-06-04 13:35:43 +0000

    fix: Reorder import statements for consistency in notification module

--- src/sakai_bot/notify/__init__.py
+++ src/sakai_bot/notify/__init__.py
@@ -1,6 +1,6 @@
 """Telegram notification module for Sakai Bot."""
 
-from sakai_bot.notify.telegram import TelegramNotifier
 from sakai_bot.notify.formatters import MessageFormatter
+from sakai_bot.notify.telegram import TelegramNotifier
 
 __all__ = ["TelegramNotifier", "MessageFormatter"]
