ffac144f17db8d85302e803934c040dcdcf942fe
Emmanuel Otoo <eotoo218@gmail.com>
2026-07-18 19:51:38 +0000

    feat: Add ResourceScraper to the scrapers module

--- src/sakai_bot/scrapers/__init__.py
+++ src/sakai_bot/scrapers/__init__.py
@@ -4,10 +4,12 @@ from sakai_bot.scrapers.announcements import AnnouncementScraper
 from sakai_bot.scrapers.assignments import AssignmentScraper
 from sakai_bot.scrapers.courses import CourseScraper
 from sakai_bot.scrapers.exams import ExamDetector
+from sakai_bot.scrapers.resources import ResourceScraper
 
 __all__ = [
     "CourseScraper",
     "AnnouncementScraper",
     "AssignmentScraper",
     "ExamDetector",
+    "ResourceScraper",
 ]
