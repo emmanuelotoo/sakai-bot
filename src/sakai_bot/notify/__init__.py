c6d50191f85a7ddf00d7535a22c9e217f38d8d13
Emmanuel Otoo <eotoo218@gmail.com>
2026-07-18 19:51:03 +0000

    feat: Update __all__ to include ReminderService and DigestService

--- src/sakai_bot/notify/__init__.py
+++ src/sakai_bot/notify/__init__.py
@@ -1,6 +1,8 @@
 """Telegram notification module for Sakai Bot."""
 
+from sakai_bot.notify.digest import DigestService
 from sakai_bot.notify.formatters import MessageFormatter
+from sakai_bot.notify.reminders import ReminderService
 from sakai_bot.notify.telegram import TelegramNotifier
 
-__all__ = ["TelegramNotifier", "MessageFormatter"]
+__all__ = ["TelegramNotifier", "MessageFormatter", "ReminderService", "DigestService"]
